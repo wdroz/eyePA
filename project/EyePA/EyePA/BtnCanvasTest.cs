705ef3a09078060d677ceeffce44660bc87b1652
William Droz <william.droz.ch@gmail.com>
2014-03-10 17:12:13 +0100

    ajout des essais pour la GUI

---
+++ project/EyePA/EyePA/BtnCanvasTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace EyePA
+{
+    class BtnCanvasTest
+    {
+        public int x,y,w,h;
+        private SolidColorBrush color;
+        public BtnCanvasTest(int x, int y, int w, int h)
+        {
+            this.x = x;
+            this.y = y;
+            this.w = w;
+            this.h = h;
+
+            this.color = Brushes.LightBlue;
+        }
+
+        public void render(Canvas cv)
+        {
+            Rectangle rect = new Rectangle();
+            rect.Height = h;
+            rect.Width = w;
+            Canvas.SetLeft(rect, x);
+            Canvas.SetRight(rect, y);
+
+            rect.Stroke = this.color;
+            rect.StrokeThickness = 2;
+
+            cv.Children.Add(rect);
+        }
+
+        public void onClique()
+        {
+            this.color = Brushes.LightGreen;
+
+        }
+    }
+}
--- project/EyePA/EyePA/MainWindow.xaml.cs
+++ project/EyePA/EyePA/MainWindow.xaml.cs
@@ -43,8 +43,20 @@ namespace EyePA
             System.Console.WriteLine(chemin);
             imageView = new ImageView(chemin);
             this.imageView.renderTo(this.GUIImageTest);
-        }
+            Image img1 = new Image();
+            Image img2 = new Image();
 
+            img1.Source = GUIImageTest.Source;
+            img2.Source = GUIImageTest.Source;
+            StackPanel sp = new StackPanel();
+            this.GUIListView.Items.Add(img1);
+            this.GUIListView.Items.Add(img2);
+
+            BtnCanvasTest toto = new BtnCanvasTest(1,1,40,40);
+
+            toto.render(this.GUIBigPicture); 
+
+        }
         
         
     }
