c3ef474cc0caad0209eeee97621b0d59e8234bd9
William Droz <william.droz.ch@gmail.com>
2014-03-10 13:06:49 +0100

    ajout des classes et interfaces (vides)

---
+++ project/EyePA/EyePA/ListView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EyePA
+{
+    public class ListView : View
+    {
+
+        private List<View> listView;
+
+        public ListView()
+        {
+            listView = new List<View>();
+        }
+
+        public void addView(View v)
+        {
+            listView.Add(v);
+        }
+    }
+}
