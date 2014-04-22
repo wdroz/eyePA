3c2984a40d2f54292910d561cb5b065ade6da2fe
William Droz <william.droz.ch@gmail.com>
2014-04-22 16:06:52 +0200

    add class for manage button to navigate

---
+++ project/EyePA/EyePA/ButtonNaviguateTo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace EyePA
+{
+    public class ButtonNaviguateTo : ButtonView
+    {
+        private String rep;
+
+        public ButtonNaviguateTo(Button btn, String rep) : base(btn)
+        {
+            this.rep = rep;
+        }
+
+        public override void addKey(System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.A)
+            {
+                //TODO faire action
+            }
+        }
+    }
+}
